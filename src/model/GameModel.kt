ff78cb81f827ae8c808928af7ba0d749968ad620
roberrera <roberterrera@gmail.com>
2017-05-29 17:11:26 -0400

    Create resetGame() and onDeckTap() functions

--- src/model/GameModel.kt
+++ src/model/GameModel.kt
@@ -10,4 +10,26 @@ class GameModel {
             , FoundationPile(diamonds), FoundationPile(hearts)
             , FoundationPile(spades))
     val tableauPiles = Array(7, { TableauPile() })
+
+    fun resetGame() {
+        wastePile.clear()
+        foundationPiles.forEach { it.reset() }
+        deck.reset()
+
+        tableauPiles.forEachIndexed { i, tableauPile ->
+            val cardsInPile: MutableList<Card> = Array(i + 1, { deck.drawCard() }).toMutableList()
+            tableauPiles[i] = TableauPile(cardsInPile)
+        }
+    }
+
+    fun onDeckTap() {
+        if (deck.cardsInDeck.size > 0) {
+            val card = deck.drawCard()
+            card.faceUp = true
+            wastePile.add(card)
+        } else {
+            deck.cardsInDeck = wastePile.toMutableList() // returns a copy of wastePile
+            wastePile.clear()
+        }
+    }
 }
